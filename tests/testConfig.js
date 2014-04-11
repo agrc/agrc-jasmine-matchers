b2d4f2e9b5ae430f80a3221ea48cd027b97a2f26
Scott Davis <stdavis@utah.gov>
2014-04-11 11:21:34 -0600

    Initial commit. All tests passing.

---
+++ tests/testConfig.js
@@ -0,0 +1,11 @@
+/* jshint unused:false */
+var dojoConfig = {
+    baseUrl: './',
+    packages: [{
+        name: 'matchers',
+        location: 'src'
+    },{
+        name: 'dojo',
+        location: 'tests/libs/dojo'
+    }]
+};
